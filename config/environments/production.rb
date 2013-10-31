aaa102a87a636a48f5c3e735e476d4ec56281a09
Gideon Kitili <emailkitili@gmail.com>
2013-10-31 16:06:02 +0300

    Add Login Slideshow images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,15 @@ Njooh::Application.configure do
     :password => "jIngahII601" ,
     :enable_starttls_auto => true
   }
+  
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
 end
